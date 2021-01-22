f45440aeda2da80d35d84c6a39cc255640c26e0c
Decebal <33691956+NisipeanuDecebalViorel@users.noreply.github.com>
2021-01-22 21:44:24 +0000

    Update life.time.sh

--- life.time.sh
+++ life.time.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#since 1976.05.20 00:00:00
-dob=$(date --date '1976-05-20 00:00:00' +%s)
+#since 1976.05.20 10:00:00
+dob=$(date --date '1976-05-20 10:00:00' +%s)
 dat=$(date +%s)
 lifeinsec=$((dat-$dob))
 
-printf '%dy:%dd:%dh:%dm:%ds\n' $(($lifeinsec/31557600)) $(($lifeinsec%31557600/86400)) $(($lifeinsec%86400/3600)) $(($lifeinsec%3600/60)) $(($lifeinsec%60))
+printf 'LifeTime [%dy]:[%dd]:[%dh]:[%dm"]:[%ds`]\n' $(($lifeinsec/31557600)) $(($lifeinsec%31557600/86400)) $(($lifeinsec%86400/3600)) $(($lifeinsec%3600/60)) $(($lifeinsec%60))
