24aa6fd172f644d03b9809a8d6008b5ca205b36d
Decebal <33691956+NisipeanuDecebalViorel@users.noreply.github.com>
2021-01-27 12:25:17 +0000

    Update life.time.sh

--- life.time.sh
+++ life.time.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
-#since 1976.05.20 10:00:00
-dob=$(date --date '1976-05-20 10:00:00' +%s)
-dat=$(date +%s)
-lifeinsec=$((dat-$dob))
-
-printf 'LifeTime [%dy]:[%dd]:[%dh]:[%dm"]:[%ds`]\n' $(($lifeinsec/31557600)) $(($lifeinsec%31557600/86400)) $(($lifeinsec%86400/3600)) $(($lifeinsec%3600/60)) $(($lifeinsec%60))
+#!/bin/bash                                                                                                                                                  
+  2 #since 1976.05.20 00:00:00                                                                                                                                   
+  3 dob=$(date --date '1976-05-20 10:00:00' +%s)                                                                                                                 
+  4 dat=$(date +%s)                                                                                                                                              
+  5 lifeinsec=$((dat-$dob))                                                                                                                                      
+  6                                                                                                                                                              
+  7 printf ' LifeTime      :  [ %dy:%dd:%d4h:%dm:%ds ]\n\n' $(($lifeinsec/31557600)) $(($lifeinsec%31557600/86400)) $(($lifeinsec%86400/3600)) $(($lifeinsec%3600
+  8 printf '            Dys.    %d.dys          :%d.hrs      :%d.min      :%d.sec\n\n' $(($lifeinsec/86400)) $(($lifeinsec%86400/3600)) $(($lifeinsec%3600/60)) $
+  9 printf '            Hrs.    %d.hrs         :%d.min      :%d.sec\n\n' $(($lifeinsec/7200)) $(($lifeinsec%3600/60)) $(($lifeinsec%60))                         
+ 10 printf '            Min.    %d.min       :%d.sec\n\n' $(($lifeinsec/60)) $(($lifeinsec%60))  
